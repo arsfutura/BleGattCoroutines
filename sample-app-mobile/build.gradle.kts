41fa1fbb950e6900c76d70ed97a5c36c86f0a587
jgrubesa <josip.grubesa10@gmail.com>
2019-11-19 14:37:31 +0100

    Revert build.gradle

--- sample-app-mobile/build.gradle.kts
+++ sample-app-mobile/build.gradle.kts
@@ -22,6 +22,14 @@ android {
     packagingOptions {
         pickFirst("META-INF/kotlinx-coroutines-core.kotlin_module")
     }
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+            storePassword = "android"
+        }
+    }
     buildTypes {
         getByName("debug") {
             applicationIdSuffix = ".debug"
