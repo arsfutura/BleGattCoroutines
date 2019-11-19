874b7bd2cd50ba9c8ae7bdf9eb3fa28da8211c3e
jgrubesa <josip.grubesa10@gmail.com>
2019-11-19 13:02:09 +0100

    Exclude sample-app-mobile module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,8 @@ include(
     ":core",
     ":genericaccess",
     ":sample-common",
-    ":sample-app-wear",
-    ":sample-app-mobile"
+    ":sample-app-wear"
+//    ":sample-app-mobile"
 )
 
 enableFeaturePreview("GRADLE_METADATA")
